e0353ab04f4d8d7d88f25bef4ce21922031e2468
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2023-04-03 16:51:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/vkmsboy/FileStreamBot-pro/tree/main ok && cd Filestreambot-pro ok && pip3 install -U -r requirements.txt
-python3 -m Adarsh
+git clone $REPO_URL ok && cd FileStreamBot-pro ok && pip3 install -U -r requirements.txt
+$START_CMD
