2e34a8f33b7b6a3cef3f61e7fdd5e5f9772da479
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2023-04-03 15:12:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 git clone https://github.com/adarsh-goel/filestreambot-pro
 cd filestreambot-pro
-virtualenv -p /usr/bin/python3 venv
-. ./venv/bin/activate
 pip install -r requirements.txt
 python3 -m Adarsh
